35eda110e72672122da7fd5f79fca7431f085265
Marcus Nilsson <marcusnilsson.webb@gmail.com>
2019-09-02 15:27:05 +0200

    Set up better state handling of login form fields.

--- src/components/TheLoginForm/TheLoginForm.jsx
+++ src/components/TheLoginForm/TheLoginForm.jsx
@@ -2,37 +2,70 @@ import React, { useState, useEffect } from 'react';
 
 import TextInput from '../../components/TextInput/TextInput';
 import BaseButton from '../../components/BaseButton/BaseButton';
-
-// Sets up the input field data.
-const formFields = [
-  {
-    key: 'accountName',
-    type: 'text',
-    variant: 'login-special',
-    placeholder: 'Account Name'
-  },
-  {
-    key: 'accountPassword',
-    type: 'password',
-    variant: 'login-special',
-    placeholder: 'Password'
-  }
-];
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const TheLoginForm = () => {
 
   let [isLoginDisabled, setIsLoginDisabled] = useState(true);
-  let [accountName, setAccountName] = useState('');
-  let [password, setPassword] = useState('');
+  let [isLoading, setIsLoading] = useState(false);
+
+  // Sets up the form-fields data.
+  let [fields, setFields] = useState([
+    {
+      key: 'accountName',
+      type: 'text',
+      variant: 'login-special',
+      placeholder: 'Account Name',
+      value: '',
+
+    },
+    {
+      key: 'accountPassword',
+      type: 'password',
+      variant: 'login-special',
+      placeholder: 'Password',
+      value: '',
+    }
+  ]);
+
+  let handleInputChange = (event, i) => {
+    let values = [...fields];
+    values[i].value = event.target.value;
+    setFields(values);
+    if(fields.every(field => field.value.length !== 0)){
+      setIsLoginDisabled(false);
+    } else {
+      setIsLoginDisabled(true);
+    }
+  };
+
+  let handleButtonClick = () => {
+    // TODO: Start login attempt here!
+    setIsLoading(true);
+    setIsLoginDisabled(true);
+  }
+
+  // Checks if we should display the loading spinner.
+  let buttonContent;
+  if(!isLoading){
+    buttonContent = <span>Login</span>
+  } else {
+    buttonContent =
+    <span>
+      <FontAwesomeIcon icon="spinner" spin />
+    </span>
+  }
 
   return (
     <div className="the-login-form">
-      {formFields.map(input => (
+      {fields.map((input, idx) => (
         <TextInput
           key={input.key}
           type={input.type}
           variant={input.variant}
+          disabled={isLoading}
           placeholder={input.placeholder}
+          onChange={e => { handleInputChange(e, idx)}}
         />
       ))}
       <div className="actions-bar">
@@ -41,8 +74,9 @@ const TheLoginForm = () => {
           variant="primary"
           buttonType="filled"
           disabled={isLoginDisabled}
+          onClick={e => {handleButtonClick()}}
         >
-          Login
+          {buttonContent}
         </BaseButton>
       </div>
     </div>
